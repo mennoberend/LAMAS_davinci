29fb0564d22810c0771abfce351e7cb99941fb94
gitaar9 <thijs.eker@gmail.com>
2020-06-04 16:51:42 +0200

    created new way of making all possible worlds

--- possible_worlds.py
+++ possible_worlds.py
@@ -2,63 +2,90 @@ from tile import Tile
 import itertools
 
 
-def permutation_adheres_to_game_rules(added_tile, previous_tile, other_player_colors, observation_string, idx):
-    if observation_string[idx] != added_tile.color:  # Make sure the color matches what we see
+class GameState:
+    def __init__(self, player_tiles, tiles_open_on_table):
+        self.player_tiles = player_tiles
+        self.flat_player_tiles = [t for p in player_tiles for t in p]
+
+        # End of every player in the flat player tiles
+        cumulative_length = 0
+        self.end_of_player_idxs = []
+        for player_length in list(map(len, player_tiles)):
+            self.end_of_player_idxs.append(cumulative_length + player_length)
+            cumulative_length += player_length
+
+        self.tiles_open_on_table = tiles_open_on_table
+
+    @property
+    def known_tiles(self):
+        return [Tile.from_string(t) for t in self.flat_player_tiles if '*' not in t] + \
+               [Tile.from_string(t) for t in self.tiles_open_on_table]
+
+    @property
+    def unknown_tiles(self):
+        return list(set(Tile.complete_set(6)) - set(self.known_tiles))
+
+    def __str__(self):
+        s = "players tiles:\n"
+        for p in player_tiles:
+            s += ' '.join(p) + '\n'
+        s += 'on table:\n' + ' '.join(self.tiles_open_on_table)
+        s += '\nknown tiles: ' + ' '.join(map(str, self.known_tiles))
+        s += '\nunknown tiles: ' + ' '.join(map(str, self.unknown_tiles))
+        return s
+
+
+def permutation_adheres_to_game_rules(added_tile, previous_tile, game_state, idx):
+    if game_state.flat_player_tiles[idx][0] != added_tile.color:  # Make sure the color matches what we see
         return False
-    not_sorted_idxs = map(len, other_player_colors)
-    if idx not in not_sorted_idxs and previous_tile is not None:
+
+    if idx not in game_state.end_of_player_idxs and previous_tile is not None:
         if previous_tile > added_tile:
             return False
+
     return True
 
 
-def perms(arr, other_player_colors, idx=0, previous_tile=None):
-    observation_string = ''.join(''.join(p) for p in other_player_colors)
-    # if len(arr) == 1:  # Base case 1, if we made all possible permutation with the supplied tiles
-    #     if permutation_adheres_to_game_rules(v, previous_tile, other_player_colors, observation_string, idx):
-    #         return [arr]
-    if idx == len(observation_string) - 1:  # Base case, we are on the required length
-        return [[v] for v in arr
-                if permutation_adheres_to_game_rules(v, previous_tile, other_player_colors, observation_string, idx)]
+def perms(arr, game_state, idx=0, previous_tile=None):
+    # We reached the required length
+    if idx == len(game_state.flat_player_tiles) - 1:
+        if '*' in game_state.flat_player_tiles[idx]:
+            return [[v] for v in arr
+                    if permutation_adheres_to_game_rules(v, previous_tile, game_state, idx)]
+        else:
+            already_known_tile = Tile.from_string(game_state.flat_player_tiles[idx])
+            if permutation_adheres_to_game_rules(already_known_tile, previous_tile, game_state, idx):
+                return [[already_known_tile]]
+            else:
+                return []
+
+    # The next tile is already known
+    if '*' not in game_state.flat_player_tiles[idx]:
+        already_known_tile = Tile.from_string(game_state.flat_player_tiles[idx])
+        if permutation_adheres_to_game_rules(already_known_tile, previous_tile, game_state, idx):
+            return [[already_known_tile] + p for p in perms(arr, game_state, idx + 1, already_known_tile)]
+        else:  # If the known tile does not adhere to the game rules, something must be wrong in this permutation
+            return []
+
     result = []
     for i, v in enumerate(arr):
-        if permutation_adheres_to_game_rules(v, previous_tile, other_player_colors, observation_string, idx):
-            result += [[v] + p for p in perms(arr[:i] + arr[i + 1:], other_player_colors, idx + 1, v)]
+        if permutation_adheres_to_game_rules(v, previous_tile, game_state, idx):
+            result += [[v] + p for p in perms(arr[:i] + arr[i + 1:], game_state, idx + 1, v)]
     return result
 
 
-def sort_combination(combination, other_player_colors):
-    sorted_combination = []
-    for sort_group_size in map(len, other_player_colors):
-        sorted_combination.extend(sorted(combination[:sort_group_size]))
-        combination = combination[sort_group_size:]
-
-    return sorted_combination
-
-
-def possible_worlds(own_tiles, other_player_colors):
-    print(f"Our own blocks: {' '.join(map(str, own_tiles))}")
-    print(f"Opponent colors: {', '.join(''.join(p) for p in other_player_colors)}\n")
-    unknown_tiles = list(set(Tile.complete_set(13)) - set(own_tiles))
-    # all_combinations = list(itertools.permutations(unknown_tiles, sum(map(len, other_player_colors))))
-    all_combinations = perms(unknown_tiles, other_player_colors)
-    print(f"There are {len(all_combinations)} possible worlds without regarding any game rules.")
-
-    # Remove duplicate worlds
-    all_combinations = [sort_combination(combination, other_player_colors) for combination in all_combinations]
-    all_combinations.sort()
-    all_combinations = list(c for c, _ in itertools.groupby(all_combinations))
-    print(f"There are {len(all_combinations)} possible worlds when removing duplicates through sorting.")
-
-    # Remove worlds that dont match with the black and white we can see
-    observation_string = ''.join(''.join(p) for p in other_player_colors)
-    all_combinations = [c for c in all_combinations if ''.join(t.color for t in c) == observation_string]
+def possible_worlds(game_state):
+    # print(f"Our own blocks: {' '.join(map(str, own_tiles))}")
+    print(f"Game state:\n{str(game_state)}")
+    print(game_state.end_of_player_idxs)
+    all_combinations = perms(game_state.unknown_tiles, game_state)
 
     print(f"There are {len(all_combinations)} possible worlds after using what we see from our opponents:")
+
     # for i, c in enumerate(all_combinations):
     #     print(f'World {i}')
     #     start_idx = 0
-    #     for players_amount_of_tiles in map(len, other_player_colors):
+    #     for players_amount_of_tiles in map(len, game_state.player_tiles):
     #         print(' '.join(str(t) for t in c[start_idx:start_idx + players_amount_of_tiles]))
     #         start_idx += players_amount_of_tiles
     #     print()
@@ -67,11 +94,25 @@ def possible_worlds(own_tiles, other_player_colors):
 if __name__ == "__main__":
     # own_tiles = list(map(lambda s: Tile.from_string(s), ['w1', 'b5', 'w5', 'w6']))
     # other_player_colors = [['b', 'b', 'w', 'w'], ['b', 'w', 'b', 'b']]
-    own_tiles = list(map(lambda s: Tile.from_string(s), ['b1', 'b4', 'b5', 'w6']))
-    other_player_colors = [['b', 'w', 'w', 'b'], ['w', 'b', 'w', 'w']]
+    player_tiles = [['w1', 'b5', 'w5', 'w6'], ['b*', 'b*', 'w*', 'w*'], ['b*', 'w*', 'b*', 'b*']]
+    game_state = GameState(player_tiles, [])
+    possible_worlds(game_state)
+
+
+    # own_tiles = list(map(lambda s: Tile.from_string(s), ['b1', 'b4', 'b5', 'w6']))
+    # other_player_colors = [['b', 'w', 'w', 'b'], ['w', 'b', 'w', 'w']]
+    player_tiles = [['b1', 'b4', 'b5', 'w6'], ['b*', 'w*', 'w*', 'b*'], ['w*', 'b*', 'w*', 'w*']]
+    game_state = GameState(player_tiles, [])
+    possible_worlds(game_state)
+
     # own_tiles = list(map(lambda s: Tile.from_string(s), ['b1', 'b4', 'b5']))
     # other_player_colors = [['b', 'w', 'w'], ['w', 'b', 'w']]
+    player_tiles = [['b1', 'b4', 'b5'], ['b*', 'w*', 'w*'], ['w*', 'b*', 'w*']]
+    game_state = GameState(player_tiles, [])
+    possible_worlds(game_state)
+
     # own_tiles = list(map(lambda s: Tile.from_string(s), ['b1', 'b4', 'b5', 'w6']))
     # other_player_colors = [['b', 'w', 'w', 'b']]
-
-    possible_worlds(own_tiles, other_player_colors)
+    player_tiles = [['b1', 'b4', 'b5', 'w6'], ['b*', 'w*', 'w*', 'b*']]
+    game_state = GameState(player_tiles, [])
+    possible_worlds(game_state)
