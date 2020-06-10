f48ec7795b12fc2029a590fcc0e9ff276f9537e0
gitaar9 <thijs.eker@gmail.com>
2020-06-10 19:21:59 +0200

    dont make plot with more than 25 worlds

--- kripke_plotter.py
+++ kripke_plotter.py
@@ -5,6 +5,10 @@ from nxpd import draw
 def build_graph(possible_worlds, G=None, color='red'):
     G = G or nx.MultiDiGraph()  # create empty graph
 
+    if len(possible_worlds) > 25:
+        print("To many worlds unable to make plot:(")
+        return G
+
     for world in possible_worlds:  # msg is python email.Message.Message object
         G.add_node(world, shape='square', penwidth=0, fontname="helvetica italic")
         other_worlds = [other for other in possible_worlds if other != world]
